59ba0eb74b1e40eacfd2d955bf9ec811641799bf
RawanGamalElDin12 <rawan.gamaleldin@outlook.com>
2023-04-24 20:36:40 +0200

    Adding bar plots of all categorical variables and displaying them in one window

--- mina.R
+++ mina.R
@@ -1,25 +1,36 @@
-data<-read.csv("Clean_Dataset.csv")
+dataset<-read.csv("Clean_Dataset.csv")
 head(dataset)
 
-
-#################
 dev.new(width = 10, height = 8)
+
+par(mfrow=c(3,3))
+#################
 numberOfOccurence<-table(dataset$source_city)
 barplot(numberOfOccurence,main = "number of occurence of source city",ylim = c(0,70000),col =" red")
 ###############
-dev.new(width = 10, height = 8)
 numberOfOccurencedest<-table(dataset$destination_city)
 barplot(numberOfOccurencedest,main = "number of occurence of destination city",ylim = c(0,70000),col =" blue")
 
 #############
-dev.new(width = 10, height = 8)
 classtable<-table(dataset$class)
-barplot(classtable/10000 ,main="frequency of economy and business flights",xlab = " flight type", ylab = "number of reservations (x10000)",ylim = c(0,30))
+barplot(classtable/10000 ,main="frequency of economy and business flights",xlab = " flight type", ylab = "number of reservations (x10000)",ylim = c(0,30),col=rainbow((6)))
 ############
 airlineTable<-table(dataset$airline)
-dev.new(width = 10, height = 8)
-barplot(airlineTable/1000 , main="number of flights of each airline",ylim = c(0,150) ,xlab = "airline name", ylab = "number of flights(x1000)")
+barplot(airlineTable/1000 , main="number of flights of each airline",ylim = c(0,150) ,xlab = "airline name", ylab = "number of flights(x1000)", col=rainbow(6))
 table(data$airline)
 
+
+#############
+departuretimeTable<-table(dataset$departure_time)
+barplot(departureTimeTable , main="Frequency of Departure Time",xlab = "Departure Time",ylab="Frequency", col=rainbow(6))
+
+#############
+ArrivaltimeTable<-table(dataset$arrival_time)
+barplot(ArrivaltimeTable , main="Frequency of Arrival Time",xlab = "Arrival Time",ylab="Frequency", col=rainbow(6))
+
+#############
+
+Stops_table<-table(dataset$stops)
+barplot(Stops_table , main="Frequency of Stops",xlab = "Stops",ylab="Frequency", col=rainbow(6))
 
 
